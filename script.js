aa24d734824bc49445fea57c73c42020edd155aa
Christopher H Jackson <jacksoch1994@gmail.com>
2021-07-01 12:32:04 -0500

    Commit after adding all documents.

---
+++ script.js
@@ -0,0 +1,95 @@
+//Get Text Field "Screen"
+
+let screen = document.getElementById("screen");
+
+//Button Functionality
+
+document.getElementById('one').onclick = () => {
+    screen.value += '1';
+    console.log(screen.value);
+}
+
+document.getElementById('two').onclick = () => {
+    screen.value += '2';
+    console.log(screen.value);
+}
+
+document.getElementById('three').onclick = () => {
+    screen.value += '3';
+    console.log(screen.value);
+}
+
+document.getElementById('four').onclick = () => {
+    screen.value += '4';
+    console.log(screen.value);
+}
+
+document.getElementById('five').onclick = () => {
+    screen.value += '5';
+    console.log(screen.value);
+}
+
+document.getElementById('six').onclick = () => {
+    screen.value += '6';
+    console.log(screen.value);
+}
+
+document.getElementById('seven').onclick = () => {
+    screen.value += '7';
+    console.log(screen.value);
+}
+
+document.getElementById('eight').onclick = () => {
+    screen.value += '8';
+    console.log(screen.value);
+}
+
+document.getElementById('nine').onclick = () => {
+    screen.value += '9';
+    console.log(screen.value);
+}
+
+document.getElementById('zero').onclick = () => {
+    screen.value += '0';
+    console.log(screen.value);
+}
+
+document.getElementById('add').onclick = () => {
+    screen.value += '+';
+    console.log(screen.value);
+}
+
+document.getElementById('subtract').onclick = () => {
+    screen.value += '-';
+    console.log(screen.value);
+}
+
+document.getElementById('multiply').onclick = () => {
+    screen.value += 'x';
+    console.log(screen.value);
+}
+
+document.getElementById('divide').onclick = () => {
+    screen.value += '/';
+    console.log(screen.value);
+}
+
+document.getElementById('decimal').onclick = () => {
+    screen.value += '.';
+    console.log(screen.value);
+}
+
+document.getElementById('carrot').onclick = () => {
+    screen.value += '^';
+    console.log(screen.value);
+}
+
+document.getElementById('left-parenthesis').onclick = () => {
+    screen.value += '(';
+    console.log(screen.value);
+}
+
+document.getElementById('right-parenthesis').onclick = () => {
+    screen.value += ')';
+    console.log(screen.value);
+}
